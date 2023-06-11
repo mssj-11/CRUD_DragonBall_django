b7b49ea062d066b722ab92ae075c405d0d1ee471
Mike <80673026+mssj-11@users.noreply.github.com>
2023-06-11 10:55:12 -0600

    Django 3.2.8
    Actualizacion de Django a la vesion 3.2.8

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Para capturar la informacion de la tabla personajes
+class Personajes(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    imagen = models.ImageField(upload_to='imagenes/', verbose_name='Imagen', null=True)
+    raza = models.CharField(max_length=100, verbose_name='Raza')
+    descripcion = models.TextField(verbose_name='Descripcion', null=True)
